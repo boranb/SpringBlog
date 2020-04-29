42d9beaad5a5f6185f06263c43713a6dd22773ea
boranb <berkayboran@gmail.com>
2020-04-29 21:48:48 +0300

    Post & Categories

---
+++ SpringBlog/ViewModel/HomeIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SpringBlog.Models;
+
+namespace SpringBlog.ViewModel
+{
+    public class HomeIndexViewModel
+    {
+        public List<Post> Posts { get; set; }
+    }
+}
