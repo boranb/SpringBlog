facccb3c845cc61b1385456c96290d89e71ba8c2
boranb <berkayboran@gmail.com>
2020-05-06 19:55:08 +0300

    Breadcrumb & nav-link

--- SpringBlog/Areas/Admin/Controllers/PostsController.cs
+++ SpringBlog/Areas/Admin/Controllers/PostsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
@@ -21,37 +22,97 @@ namespace SpringBlog.Areas.Admin.Controllers
         // GET: Admin/Posts
         public ActionResult New()
         {
-            ViewBag.CategoryId = new SelectList(db.Categories.OrderBy(x=>x.CategoryName).ToList(),"Id","CategoryName");
+            ViewBag.CategoryId =
+                new SelectList(db.Categories.OrderBy(x => x.CategoryName).ToList(), "Id", "CategoryName");
             return View();
         }
-
-        [HttpPost,ValidateAntiForgeryToken]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult New(NewPostViewModel vm)
         {
             if (ModelState.IsValid)
             {
-                Post post = new Post()
+                Post post = new Post
                 {
                     CategoryId = vm.CategoryId,
                     Title = vm.Title,
                     Content = vm.Content,
                     AuthorId = User.Identity.GetUserId(),
-                    Slug = UrlService.URLFriendly(vm.Slug), // url'yi title'a göre üretiyor
+                    Slug = UrlService.URLFriendly(vm.Slug),
                     CreationTime = DateTime.Now,
                     ModificationTime = DateTime.Now,
                     PhotoPath = this.SaveImage(vm.FeaturedImage)
                 };
-
                 db.Posts.Add(post);
                 db.SaveChanges();
-                
+                TempData["SuccessMessage"] = "The post has been created successfully!";
                 return RedirectToAction("Index");
             }
 
+            ViewBag.CategoryId =
+                new SelectList(db.Categories.OrderBy(x => x.CategoryName).ToList(), "Id", "CategoryName");
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var post = db.Posts.Find(id);
+
+            if (post == null)
+            {
+                return HttpNotFound();
+            }
+
+            var vm = new EditPostViewModel()
+            {
+                Id = post.Id,
+                CategoryId = post.CategoryId,
+                Content = post.Content,
+                CreationTime = post.CreationTime.Value,
+                CurrentFeaturedImage = post.PhotoPath,
+                ModificationTime = post.ModificationTime.Value,
+                Slug = post.Slug,
+                Title = post.Title
+            };
+
+            ViewBag.CategoryId = new SelectList(db.Categories.OrderBy(x => x.CategoryName).ToList(), "Id", "CategoryName");
+            return View(vm);
+        }
+
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Edit(EditPostViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var post = db.Posts.Find(vm.Id);
+                post.CategoryId = vm.CategoryId;
+                post.Title = vm.Title;
+                post.Content = vm.Content;
+                post.ModificationTime = DateTime.Now;
+                post.Slug = UrlService.URLFriendly(vm.Slug);
+
+                if (vm.FeaturedImage != null)
+                {
+                    this.DeleteImage(post.PhotoPath);
+                    post.PhotoPath = this.SaveImage(vm.FeaturedImage);
+                }
+
+                db.SaveChanges();
+                TempData["SuccessMessage"] = "The post has been updated successfully.";
+
+                return RedirectToAction("Index");
+            }
 
             ViewBag.CategoryId = new SelectList(db.Categories.OrderBy(x => x.CategoryName).ToList(), "Id", "CategoryName");
             return View();
         }
+
+
 
         [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Delete(int id)
@@ -63,6 +124,7 @@ namespace SpringBlog.Areas.Admin.Controllers
                 return HttpNotFound();
             }
 
+            this.DeleteImage(post.PhotoPath);
             db.Posts.Remove(post);
             db.SaveChanges();
             TempData["SuccessMessage"] = "The post deleted successfully.";
