5a9376d5094447770b30a576fbacf3f73f430b50
boranb <berkayboran@gmail.com>
2020-05-12 19:40:34 +0300

    Comment approve

---
+++ SpringBlog/Areas/Admin/Controllers/CommentsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SpringBlog.Controllers;
+using SpringBlog.Enums;
+
+namespace SpringBlog.Areas.Admin.Controllers
+{
+    public class CommentsController : AdminBaseController
+    {
+        // GET: Admin/Comments
+        public ActionResult Index()
+        {
+            return View(db.Comments.ToList()); // datatable kullandığımız için direkt listeye aktardık
+            //return View(db.Comments.OrderByDescending(x=>x.CreationTime).ToList()); 
+        }
+        [HttpPost]
+        public ActionResult ChangeState(int id, bool isPublished)
+        {
+            var comment = db.Comments.Find(id);
+            comment.State = isPublished ? CommentState.Approved : CommentState.Rejected;
+            db.SaveChanges();
+            return new HttpStatusCodeResult(HttpStatusCode.OK);
+        }
+    }
+}
