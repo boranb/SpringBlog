a5a6e6805ecbda743ec3fc6f464dda6c9458bab4
boranb <berkayboran@gmail.com>
2020-05-13 22:36:21 +0300

    App secrets

--- SpringBlog/Helpers/IdentityHelpers.cs
+++ SpringBlog/Helpers/IdentityHelpers.cs
@@ -5,6 +5,7 @@ using System.Security.Claims;
 using System.Security.Principal;
 using System.Web;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 using SpringBlog.Models;
 
 namespace SpringBlog.Helpers
@@ -15,10 +16,11 @@ namespace SpringBlog.Helpers
         {
             var claims = ((ClaimsIdentity) identity).Claims;
 
-           string displayName = claims.FirstOrDefault(x => x.Type == "DisplayName").Value;
+            string displayName = claims.FirstOrDefault(x => x.Type == "DisplayName").Value;
 
             return displayName;
         }
+
         public static string ProfilePhoto(this IIdentity identity)
         {
             using (var db = new ApplicationDbContext())
@@ -27,6 +29,77 @@ namespace SpringBlog.Helpers
                 var user = db.Users.Find(userId);
                 return user.ProfilePhoto;
             }
+        }
+
+        public static void SeedRolesAndUsers()
+        {
+            var context = new ApplicationDbContext();
+
+            // https://stackoverflow.com/questions/19280527/mvc-5-seed-users-and-roles
+            if (!context.Roles.Any(r => r.Name == "admin"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole {Name = "admin"};
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "boran@bekoo.co"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser
+                {
+                    UserName = "boran@bekoo.co",
+                    Email = "boran@bekoo.co",
+                    DisplayName = "Boran",
+                    EmailConfirmed = true
+                };
+
+                manager.Create(user, "Password1.");
+                manager.AddToRole(user.Id, "admin");
+
+                #region Seed Categories and Posts
+
+                if (!context.Categories.Any())
+                {
+                    context.Categories.Add(new Category
+                    {
+                        CategoryName = "Sample Category 1",
+                        Slug = "sample-category-1",
+                        Posts = new List<Post>
+                        {
+                            new Post
+                            {
+                                Title = "Sample Post 1",
+                                AuthorId = user.Id,
+                                Content =
+                                    "<p>Tincidunt integer eu augue augue nunc elit dolor, luctus placerat scelerisque euismod, iaculis eu lacus nunc mi elit, vehicula ut laoreet ac, aliquam sit amet justo nunc tempor, metus vel.</p>",
+                                Slug = "sample-post-1",
+                                CreationTime = DateTime.Now,
+                                ModificationTime = DateTime.Now
+                            },
+                            new Post
+                            {
+                                Title = "Sample Post 2",
+                                AuthorId = user.Id,
+                                Content =
+                                    "<p>Tincidunt integer eu augue augue nunc elit dolor, luctus placerat scelerisque euismod, iaculis eu lacus nunc mi elit, vehicula ut laoreet ac, aliquam sit amet justo nunc tempor, metus vel.</p>",
+                                Slug = "sample-post-2",
+                                CreationTime = DateTime.Now,
+                                ModificationTime = DateTime.Now
+                            }
+                        }
+                    });
+                }
+
+                #endregion Seed Categories and Posts
+
+            }
+
+            context.SaveChanges();
+            context.Dispose();
         }
     }
 }
--- SpringBlog/Migrations/Configuration.cs
+++ SpringBlog/Migrations/Configuration.cs
@@ -16,69 +16,9 @@ namespace SpringBlog.Migrations
             AutomaticMigrationsEnabled = false;
         }
 
-        // https://stackoverflow.com/questions/19280527/mvc-5-seed-users-and-roles
 
         protected override void Seed(SpringBlog.Models.ApplicationDbContext context)
         {
-            if (!context.Roles.Any(r => r.Name == "admin"))
-            {
-                var store = new RoleStore<IdentityRole>(context);
-                var manager = new RoleManager<IdentityRole>(store);
-                var role = new IdentityRole { Name = "admin" };
-
-                manager.Create(role);
-            }
-
-            if (!context.Users.Any(u => u.UserName == "boran@bekoo.co"))
-            {
-                var store = new UserStore<ApplicationUser>(context);
-                var manager = new UserManager<ApplicationUser>(store);
-                var user = new ApplicationUser
-                {
-                    UserName = "boran@bekoo.co",
-                    Email = "boran@bekoo.co",
-                    DisplayName = "Boran",
-                    EmailConfirmed = true
-                };
-
-                manager.Create(user, "Password1.");
-                manager.AddToRole(user.Id, "admin");
-
-                #region Seed Categories and Posts
-
-                if (!context.Categories.Any())
-                {
-                    context.Categories.Add(new Category
-                    {
-                        CategoryName = "Sample Category 1",
-                        Slug = "sample-category-1",
-                        Posts = new List<Post>
-                        {
-                            new Post
-                            {
-                                Title = "Sample Post 1",
-                                AuthorId = user.Id,
-                                Content = "<p>Tincidunt integer eu augue augue nunc elit dolor, luctus placerat scelerisque euismod, iaculis eu lacus nunc mi elit, vehicula ut laoreet ac, aliquam sit amet justo nunc tempor, metus vel.</p>",
-                                Slug = "sample-post-1",
-                                CreationTime = DateTime.Now,
-                                ModificationTime = DateTime.Now
-                            },
-                            new Post
-                            {
-                                Title = "Sample Post 2",
-                                AuthorId = user.Id,
-                                Content = "<p>Tincidunt integer eu augue augue nunc elit dolor, luctus placerat scelerisque euismod, iaculis eu lacus nunc mi elit, vehicula ut laoreet ac, aliquam sit amet justo nunc tempor, metus vel.</p>",
-                                Slug = "sample-post-2",
-                                CreationTime = DateTime.Now,
-                                ModificationTime = DateTime.Now
-                            }
-                        }
-                    });
-                }
-
-                #endregion Seed Categories and Posts
-            }
-
             #region GeneratePosts
 
             if (!context.Categories.Any(x => x.CategoryName == "Generated Posts") && context.Users.Any())
